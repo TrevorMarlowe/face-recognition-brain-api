6825b8503773f42505778eafb8869b764faf18cb
Trevor Marlowe <marlowetrevor@hotmail.com>
2019-05-18 00:55:48 -0600

    updated files

--- controllers/image.js
+++ controllers/image.js
@@ -7,10 +7,9 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
 	app.models
 		.predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
-		// .then(data => {
-		// 	res.json(data)
-		// 	//console.log(data)
-		// })
+		.then(data => {
+			res.json(data)
+		})
 		.catch(err => res.status(400).json('Unable to work with API')) 
 }
 
