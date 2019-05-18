3383b741d3c7fe228adc583a0d9007d15450f65a
Trevor Marlowe <marlowetrevor@hotmail.com>
2019-05-18 16:58:23 -0600

    passed saltRounds to handleRegister

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 
 app.get('/', (req, res) => { res.send('it is working') });
 app.post('/signin', signin.handleSignin(db, bcrypt));
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt, saltRounds)});
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
