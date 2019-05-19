2e77dca7a05b57ef794d7dc65ed06bbefcd037f0
Trevor Marlowe <marlowetrevor@hotmail.com>
2019-05-18 19:12:10 -0600

    removed api key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: '04b9eb41801040cb8f30001f40b101ee'
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
